761b2604ae2d64998d296a6be228fc86ca2d7ce4
finesketch <coder@Kyles-MacBook-Pro.local>
2016-09-06 10:41:34 +0800

    Chapter13

--- BigNerdRanchBook/Chapter13/TimeAfterTime/TimeAfterTime/main.m
+++ BigNerdRanchBook/Chapter13/TimeAfterTime/TimeAfterTime/main.m
@@ -15,6 +15,12 @@ int main(int argc, const char * argv[]) {
         
         NSDate *now = [NSDate date];
         NSLog(@"This NSDate object lives at %p", now);
+        NSLog(@"The date is %@", now);
+        
+        double seconds = [now timeIntervalSince1970];
+        NSLog(@"It has been %f seconds since the start of 1970", seconds);
+        
+        //NSDate *testNow = [now date]; // error
     }
     return 0;
 }
